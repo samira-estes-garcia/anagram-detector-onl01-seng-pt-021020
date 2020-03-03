a73f2218de260ab9c266b78857db7f54b270ef20
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-03 15:28:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,17 +7,7 @@ class Anagram
   end
   
   def match(anagram_arr)
-    split_elements = []
-    anagram_arr.each do |element|
-      split_elements << element.split("")
-    end
-    sorted_elements = []
-    split_elements.each do |element|
-      sorted_elements << element.sort
-    end
-    sorted_elements.each do |element|
-      element == word.sort ? element : [ ] 
-    end
+    
   end
   
 end
