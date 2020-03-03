72dc2cbf996b5ec2d2c87422eb1cd9016efa8c41
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-03 15:16:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagram_arr)
+    anagram_arr.collect do |element|
+      split_elements = element.split("")
+    end
+    split_elements
+  end
+  
+end
+ #split_element.sort == word.sort ? element : [ ] 
